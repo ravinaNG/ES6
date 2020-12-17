95806476e34d5211eedbbe10223df37b71deaf74
Ravina Kumari <ravina18@navgurukul.org>
2020-12-17 07:27:19 +0530

    difference between var, let and const

---
+++ varLetConst.js
@@ -0,0 +1,19 @@
+if(true){
+    //this 'if' condition block doesn't create a scope
+    
+    //name is in the global scope because of the 'var' keyword
+    var name = 'Hammad';
+
+    //likes is in the local scope because of the 'let' keyword
+    let likes = 'Coding';
+
+    //skills is in the local scope because of the 'const' keyword
+    const skills = 'JavaScript and PHP' 
+}
+
+console.log(name); //logs 'Hammad'
+console.log(likes); //Uncaught ReferenceError: likes is not defined
+console.log(skills); //Uncaugth ReferenceError: skills is not defined
+
+
+//Global scope lives as long as your application lives. Local scope lives as long as your functions are calles and execute.
